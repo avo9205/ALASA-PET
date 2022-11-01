0b157b1481c4cb68e8d2aa7f7df4bb2b70602068
Sergio Muñoz <sergiolopera.94@gmail.com>
2022-11-01 15:51:56 -0500

    Creada la plantilla para la tienda

--- alasa-pet/src/App.js
+++ alasa-pet/src/App.js
@@ -1,23 +1,35 @@
-import logo from './logo.svg';
 import './App.css';
+import Banner from './components/Banner'
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import {faCartShopping} from '@fortawesome/free-solid-svg-icons'
+
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+      <header className="contenedor-header">
+        <div className='texto-left'>Tienda de mascotas - alasa pet</div>
+        <div className='texto-right'>Contactar - 4446950</div>
+        <div className='logo'>
+          <a href='/'><img className="logo-imagen" 
+          src={require("./images/logo.png")} alt='logo'/></a>
+        </div>
+        <div className='contenedor-busqueda'>
+          <div className='contenedor-form'>
+            <form className='form-busqueda'>
+              <input className='input' itemType='text' placeholder='Búsqueda en catálogo'/>
+            </form>
+          </div>
+        </div>
+        <div className='carrito'>
+          <a href='/'>
+            <FontAwesomeIcon icon={faCartShopping}/>
+          </a>
+        </div>
       </header>
+
+
+      <Banner/>
     </div>
   );
 }
---
+++ alasa-pet/src/components/Banner.js
@@ -0,0 +1,15 @@
+import React from "react";
+import '../css/Banner.css';
+
+function Banner() {
+  return (
+    <div className="banner">
+      <img className="imagen-producto" src={require("../images/1.jpg")} alt='Foto'/>
+      <div className="contenedor-producto">
+
+      </div>
+    </div>
+  );
+}
+
+export default Banner;
